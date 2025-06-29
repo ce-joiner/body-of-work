c382d79b8975faa0e8054ec2ceab79e9290db05e
Casey Joiner <caseyejoiner@gmail.com>
2025-06-29 12:41:24 -0500

    Add PhotoAdmin class with bulk actions and file size display

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Photo
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
@@ -23,3 +23,53 @@ class ProjectAdmin(admin.ModelAdmin):
         return obj.is_overdue()
     is_overdue.boolean = True
     is_overdue.short_description = 'Overdue?'
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'uploaded_at', 'is_featured', 'needs_attention', 'file_size_display')
+    list_filter = ('project', 'is_featured', 'needs_attention', 'uploaded_at', 'mime_type')
+    search_fields = ('title', 'caption', 'project__title')
+    readonly_fields = ('uploaded_at', 'updated_at', 'file_size', 'mime_type', 'width', 'height')
+    ordering = ('-uploaded_at',)
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'project', 'image', 'caption')
+        }),
+        ('Status', {
+            'fields': ('is_featured', 'needs_attention', 'order_index')
+        }),
+        ('Technical Info', {
+            'fields': ('uploaded_at', 'updated_at', 'file_size', 'mime_type', 'width', 'height'),
+            'classes': ('collapse',)  # This section will be collapsed by default
+        }),
+    )
+    
+    def file_size_display(self, obj):
+        """Display file size in human readable format"""
+        return obj.file_size_human
+    file_size_display.short_description = 'File Size'
+    
+    # Add actions for bulk operations
+    actions = ['mark_featured', 'unmark_featured', 'mark_attention', 'unmark_attention']
+    
+    def mark_featured(self, request, queryset):
+        updated = queryset.update(is_featured=True)
+        self.message_user(request, f'{updated} photos marked as featured.')
+    mark_featured.short_description = "Mark selected photos as featured"
+    
+    def unmark_featured(self, request, queryset):
+        updated = queryset.update(is_featured=False)
+        self.message_user(request, f'{updated} photos unmarked as featured.')
+    unmark_featured.short_description = "Remove featured status from selected photos"
+    
+    def mark_attention(self, request, queryset):
+        updated = queryset.update(needs_attention=True)
+        self.message_user(request, f'{updated} photos marked as needing attention.')
+    mark_attention.short_description = "Mark selected photos as needing attention"
+    
+    def unmark_attention(self, request, queryset):
+        updated = queryset.update(needs_attention=False)
+        self.message_user(request, f'{updated} photos unmarked as needing attention.')
+    unmark_attention.short_description = "Remove attention flag from selected photos"
